6ad6d311e91a574836e9fd8d0f0fafe792f7a39f
ibhf13 <ibhf13@gmail.com>
2024-09-29 13:47:39 +0200

    app name

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,30 +1,33 @@
 import { defineConfig } from '@rsbuild/core';
-   import { pluginReact } from '@rsbuild/plugin-react';
+import { pluginReact } from '@rsbuild/plugin-react';
 
-   export default defineConfig({
-     plugins: [pluginReact()],
-     output: {
-       distPath: {
-         root: 'dist',
-         js: 'static/js',
-         css: 'static/css',
-         image: 'static/images',
-         fonts: 'static/fonts',
-       },
-     },
-     tools: {
-       rspack: {
-         optimization: {
-           splitChunks: {
-             cacheGroups: {
-               vendor: {
-                 test: /[\\/]node_modules[\\/]/,
-                 name: 'vendors',
-                 chunks: 'all',
-               },
-             },
-           },
-         },
-       },
-     },
-   });
+export default defineConfig({
+  plugins: [pluginReact()],
+  html: {
+    title: 'Portfolio',
+  },
+  output: {
+    distPath: {
+      root: 'dist',
+      js: 'static/js',
+      css: 'static/css',
+      image: 'static/images',
+      fonts: 'static/fonts',
+    },
+  },
+  tools: {
+    rspack: {
+      optimization: {
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+              chunks: 'all',
+            },
+          },
+        },
+      },
+    },
+  },
+});
